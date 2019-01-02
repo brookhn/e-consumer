cbe829979c35f86be93bb3ff0fb3ac4ec100ffa5
yangtao <18111987@suningcultural.com>
2019-01-02 17:18:24 +0800

    test

---
+++ src/main/java/com/pp/server/EConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.pp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EConsumerApplication.class, args);
+	}
+
+}
+
